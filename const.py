981b6ef4ec66278403d6e0049e2b15ea82b97b03
ignchap27 <i.chaparro@uniandes.edu.co>
2026-09-10 10:23:19 -0500

    data

--- const.py
+++ const.py
@@ -1,2 +1,2 @@
 data_path_5 = "https://drive.google.com/file/d/1jw4BeYff38HANTT3rAP8VdNcJD-4OJPC/view?usp=sharing"
-data_path_4 = "https://drive.google.com/file/d/1HM3ITs6z0KqHojEBvR9ddUhlUU_-Mc6K/view?usp=sharing"
+# data_path_4 = "https://drive.google.com/file/d/1HM3ITs6z0KqHojEBvR9ddUhlUU_-Mc6K/view?usp=sharing"
